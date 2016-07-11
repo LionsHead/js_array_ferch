8e35e57f7ebc6acce4d55336ffff5db49306abbe
Александр <lion2head@gmail.com>
2016-07-11 23:04:12 +0400

    use babelrc for mocha

--- exercise/test.js
+++ exercise/test.js
@@ -1,6 +1,12 @@
 import assert from 'assert';
 import arrayFetch from './solution';
 
-assert.equal('b', arrayFetch(['a', 'b', 'c', 'd'], 1, 'e'));
-assert.equal('c', arrayFetch(['a', 'b', 'c', 'd'], -1, 'e'));
-assert.equal('e', arrayFetch(['a', 'b', 'c', 'd'], 12, 'e'));
+describe('arrayFetch', () => {
+  it('should work', () => {
+    assert.equal('b', arrayFetch(['a', 'b', 'c', 'd'], 1, 'e'));
+    assert.equal('c', arrayFetch(['a', 'b', 'c', 'd'], -1, 'e'));
+  });
+  it('default value', () => {
+    assert.equal('e', arrayFetch(['a', 'b', 'c', 'd'], 12, 'e'));
+  });
+});
