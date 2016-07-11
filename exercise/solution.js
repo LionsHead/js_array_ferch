9998826387d716cd37b4c2b45397618a130317df
Александр <lion2head@gmail.com>
2016-07-11 20:04:06 +0400

    eslint  remarks

--- exercise/solution.js
+++ exercise/solution.js
@@ -1,11 +1,12 @@
 // BEGIN (write your solution here)
-const arrayFetch = (arr, index, def) => {
+const arrayFetch = (arr, id, def) => {
+  let index = id;
   if (index < 0) {
     arr.reverse();
     index = Math.abs(index);
   }
 
   return (arr[index] !== undefined) ? arr[index] : def;
-}
+};
 // END
 export default arrayFetch;
