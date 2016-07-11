9fed973a423b26bf0eda94300b742632da568955
Александр <lion2head@gmail.com>
2016-07-11 13:06:04 +0400

    example

--- exercise/test.js
+++ exercise/test.js
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import solution from './solution';
 
-assert.equal('b', solution.arrayFetch(["a", "b", "c"], 1, "d"]));
-assert.equal('c', solution.arrayFetch(["a", "b", "c"], -1, "d"]));
+assert.equal('b', solution.arrayFetch(['a', 'b', 'c', 'd'], 1, 'e'));
+assert.equal('c', solution.arrayFetch(['a', 'b', 'c', 'd'], -1, 'e'));
+assert.equal('e', solution.arrayFetch(['a', 'b', 'c', 'd'], -12, 'e'));
